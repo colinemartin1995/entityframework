0d7a2e1ad291076653c7b4dd21f2a39059dec11a
Coline Martin <colinemartin95@gmail.com>
2017-01-30 11:39:21 +0100

    Changer le nom d'un champs dans la bdd avec Add-Migration ChangeDisplayName + Update-Database

--- CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/Program.cs
+++ CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/Program.cs
@@ -85,6 +85,13 @@ namespace CodeFirstNewDatabaseSample
             public DbSet<Post> Posts { get; set; }
             public DbSet<Tag> Tags { get; set; }
             public DbSet<User> Users { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<User>()
+                    .Property(u => u.DisplayName)
+                    .HasColumnName("display_name");
+            }
         }
 
 
